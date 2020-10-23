d01c5bd7348b1161d28dc46ca2a080ded9af404a
Dana Bliss <danajackson2@gmail.com>
2020-10-23 20:44:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@ require 'pry'
 def run_guessing_game
   a = rand(1...6)
   puts "Guess a number between 1 and 6."
-  b = gets.chomp.to_i
+  b = gets.chomp
 binding.pry
-  if a = b
+  if a == b
     puts "You guessed the correct number!"
   elsif a != b
     puts "Sorry! The computer guessed #{a}."
