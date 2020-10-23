05548f175031d1877d546165c4d166a3b63614e1
Dana Bliss <danajackson2@gmail.com>
2020-10-23 20:33:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,12 @@
 def run_guessing_game
-  # genereat
+  # generate and store rand btwn 1-6
+  
+  # prompts user to guess a number
+  
+  #gets user input from command line
+  
+  #compare input to rand number
+  
+  #prints either ""
+  
 end  
